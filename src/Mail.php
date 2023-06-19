b678f8f69cb210a3ce0aa991f34abd56104c7d54
Наранхүү <codesaur@gmail.com>
2023-06-19 11:21:50 +0800

    small changes

--- src/Mail.php
+++ src/Mail.php
@@ -6,18 +6,12 @@ use PHPMailer\PHPMailer\PHPMailer;
 
 class Mail
 {
-    public function send(string|array $from, string|array $to, string $subject, string $message): bool
+    public function send(string $from, string $to, string $subject, string $message): bool
     {
         if (empty($from)) {
             throw new \InvalidArgumentException('Mail sender must be set!');
-        } elseif (\is_array($from)) {
-            $sender = '=?UTF-8?B?' . \base64_encode($from[0]) . '?= <' . $from[1] . '>';
-        }
-  
-        if (empty($to)) {
+        } elseif (empty($to)) {
             throw new \InvalidArgumentException('Mail recipient must be set!');
-        } elseif (\is_array($to)) {
-            $recipient = '=?UTF-8?B?' . \base64_encode($to[0]) . '?= <' . $to[1] . '>';
         }
         
         if (empty($subject) || empty($message)) {
@@ -31,15 +25,12 @@ class Mail
         $header  = 'MIME-Version: 1.0' . "\r\n";
         $header .= 'Content-type: ' . $content_type . '; charset=utf-8' . "\r\n";
         $header .= 'Content-Transfer-Encoding: base64' . "\r\n";
-        $header .= 'Date: ' . \date('r (T)') . "\r\n";
-        
-        $from_addr = $from ?? $sender;
-        $header .= 'From: ' . $from_addr . "\r\n";
-        $header .= 'Reply-To: ' . $from_addr . "\r\n";
-        
+        $header .= 'Date: ' . \date('r (T)') . "\r\n";        
+        $header .= 'From: ' . $from . "\r\n";
+        $header .= 'Reply-To: ' . $from . "\r\n";        
         $header .= 'X-Mailer: PHP/' . \phpversion();
         
-        if (\mail($recipient ?? $to, $subject, \base64_encode($message), $header)) {
+        if (\mail($to, $subject, \base64_encode($message), $header)) {
             return true;
         }
         
@@ -68,7 +59,7 @@ class Mail
         
         $mailer = new PHPMailer($exceptions);
         if ($is_smtp) {
-            $mailer->IsSMTP();
+            $mailer->isSMTP();
         }
         $mailer->Mailer = 'smtp';
         $mailer->CharSet = $charset;
@@ -78,17 +69,17 @@ class Mail
         $mailer->Port = $port;
         $mailer->Username = $username;
         $mailer->Password = $password;
-        $mailer->SetFrom($from, $from_name);
-        $mailer->AddReplyTo($from, $from_name);
+        $mailer->setFrom($from, $from_name);
+        $mailer->addReplyTo($from, $from_name);
         $mailer->SMTPOptions = $smtp_options;
-        $mailer->MsgHTML($message);
+        $mailer->msgHTML($message);
         $mailer->Subject = $subject;
-        $mailer->AddAddress($to, $to_name);
+        $mailer->addAddress($to, $to_name);
         
         if (!empty($lang_code)) {
             $mailer->setLanguage($lang_code);
         }
         
-        return $mailer->Send();
+        return $mailer->send();
     }
 }
